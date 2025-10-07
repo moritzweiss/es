26ff6fdf5e9e437752667a7c450fcc8b7c46cd65
Moritz Weiss <moritz.weiss@math.ehtz.ch>
2025-10-07 19:04:57 +0200

    update expected shortfall

--- expected_shortfall.py
+++ expected_shortfall.py
@@ -6,8 +6,8 @@ def expected_shortfall(
     losses: torch.Tensor,
     alpha: float = 0.95,
     sample_weights: Optional[torch.Tensor] = None, 
-    normalization = False,
-    is_decreasing = False ) -> Tuple[torch.tensor, int]:
+    normalize = False,
+    make_decreasing = False ) -> Tuple[torch.tensor, int]:
     """
     This implements the estimator: ES_hat = (1/(1-alpha)) [ sum_{i=1}^{j-1} w_(i) L_(i) + ( 1 - (sum_{i=1}^{j-1} w_(i)) / (1-alpha) ) * L_(j) ]
     where L_(1) ≥ ... ≥ L_(N) are order stats and j is the first index whose cumulative weight exceeds (1-alpha). 
@@ -34,11 +34,11 @@ def expected_shortfall(
     else:
         assert sample_weights.shape == losses.shape
         assert torch.all(sample_weights>=0), "weights must be positive"        
-        if normalization:
+        if normalize:
             sample_weights = sample_weights / sample_weights.sum()
         else:
             assert torch.isclose(sample_weights.sum(), torch.tensor(1.0, dtype=sample_weights.dtype, device=sample_weights.device), rtol=1e-2), "weights must not sum to one"
-    if not is_decreasing:
+    if make_decreasing:
         losses, idx = torch.sort(losses, dim=-1, descending=True)
         sample_weights = torch.gather(input=sample_weights,dim=0,index=idx)
 
@@ -63,14 +63,14 @@ if __name__ == "__main__":
     losses = torch.tensor([10., 8., 8., 8., 5., 4., 3., 2., 1., 0.])
     alpha = 0.7
     es = losses[:3].sum()/3
-    out, j = expected_shortfall(losses=losses, alpha=alpha, normalization=normalization)
+    out, j = expected_shortfall(losses=losses, alpha=alpha, normalize=normalization)
     assert torch.isclose(out, es, rtol=1e-5)
     assert j == 3 
     # b) an example with ties 
     alpha = 0.75
     j = 2 # 0 based indexing shifts by -1
     es = losses[:j].sum()/(10*(1-alpha)) + (1 - j/(10*(1-alpha))) * losses[j]
-    es_out, j_out = expected_shortfall(losses=losses, alpha=alpha, normalization=normalization)
+    es_out, j_out = expected_shortfall(losses=losses, alpha=alpha, normalize=normalization)
     assert torch.isclose(es_out, es, rtol=1e-5)    
     assert j_out == j
     # c) example with weights, but not normalized  
@@ -80,8 +80,8 @@ if __name__ == "__main__":
         losses=losses,
         alpha=alpha,
         sample_weights=weights,
-        normalization=False,        
-        is_decreasing=True         
+        normalize=False,        
+        make_decreasing=True         
     )
     assert torch.isclose(es_w, torch.tensor(10, dtype=torch.float32), rtol=1e-6, atol=0.0)
     assert abs(j_w-0) < 1e-1
@@ -89,7 +89,7 @@ if __name__ == "__main__":
     weights = torch.tensor([0.20, 0.18, 0.15, 0.12, 0.10, 0.08, 0.07, 0.05, 0.03, 0.02])
     assert torch.isclose(weights.sum(), torch.tensor(1.0))  
     alpha = 0.7
-    es_w, j_w = expected_shortfall(losses=losses,alpha=alpha, sample_weights=weights, normalization=False, is_decreasing=True)
+    es_w, j_w = expected_shortfall(losses=losses,alpha=alpha, sample_weights=weights, normalize=False, make_decreasing=True)
     # j=2 therefore j=1 in python. the below only concerns the first value. 
     first_term = (losses[0]*weights[0])/(1-alpha)
     second_term = (1-weights[0]/(1-alpha))*losses[1]
