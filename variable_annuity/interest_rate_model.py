a9053da094e5e15b8600aa0afc66a6d9a5f01bd0
Moritz Weiss <moritz.weiss@math.ehtz.ch>
2025-10-10 13:33:03 +0200

    update interest rate model

--- variable_annuity/interest_rate_model.py
+++ variable_annuity/interest_rate_model.py
@@ -15,6 +15,13 @@ class AnnuityConfig:
     rho: float = -0.0645
     startx: float = 0.0
     starty: float = 0.0
+    # for the G2++ model
+    beta0: float = 1.54617/100
+    beta1: float = 2.47537/100
+    beta2: float = -26.22674/100
+    beta3: float = 26.04199/100
+    theta1: float = 4.80792/100
+    theta2: float = 5.83035/100     
 
 def _to_tensor(value, device, dtype):
     if isinstance(value, float): 
@@ -47,12 +54,67 @@ class TwoFactorOrnsteinUhlenbeckModel:
         y = self._single_increment(start_y, dt, reversion=self.beta, volatility=self.nu, normal_draws=self.rho*normal_1 + torch.sqrt(1-self.rho**2)*normal_2)
         return x, y
 
+class G2PPModel:
+    def __init__(self, config, seed, device='cpu', dtype=torch.float32):
+        self.rng = torch.Generator().manual_seed(seed)
+        self.device = device
+        self.dtype = dtype
+        for f in fields(config):
+            value = getattr(config, f.name)
+            value = _to_tensor(value, device=self.device, dtype=self.dtype)
+            setattr(self, f.name, value)
+        # some attributes are not needer for OU, but we pass them anyway 
+        self.coupled_ou = TwoFactorOrnsteinUhlenbeckModel(config, seed, device, dtype)
+
+    def f(self, t):
+        assert t >= 0
+        if t == 0:
+            return self.beta0 
+        else:
+            factor = lambda t, a, b: torch.exp(-t/a)*(t+b)/b
+            first_term = self.beta0
+            second_term = self.beta1*(self.theta1/t)*(1 - torch.exp(-t/self.theta1))
+            third_term = self.beta2*(self.theta1/t)*(1 - factor(t, self.theta1, self.theta1))
+            fourth_term = self.beta3*(self.theta2/t)*(1 - factor(t, self.theta1, self.theta2))
+            return first_term + second_term + third_term + fourth_term
+    
+    def g(self, kappa, t):
+        return torch.sqrt((1 - torch.exp(-kappa*t)))*(1/kappa)
+
+    def psi(self, t):
+        term1 = self.f(t) 
+        term2 = (self.nu**2/2)*self.g(self.alpha, t)**2
+        term3 = (self.eta**2/2)*self.g(self.beta, t)**2
+        term4 = self.rho*self.nu*self.eta*self.g(self.alpha, t)*self.g(self.beta, t)
+        return term1 + term2 + term3 + term4
+    
+    def interest_rate(self, dt, x_start, y_start, t_start):
+        # simulate the process at time t_start + dt given the values of x and y at time t_start
+        x, y = self.coupled_ou.coupled_increment(x_start, y_start, dt)        
+        return self.psi(t_start+dt) + x + y
+    
+    def interest_rate_path(self, n_steps, x_start, y_start, t_start, dt):
+        # x,y,z at time t_start
+        x_path = [x_start]
+        y_path = [y_start]
+        r_path = [self.interest_rate(0, x_start, y_start, t_start)]
+        for step in range(1, n_steps):
+            x, y = self.coupled_ou.coupled_increment(x_path[-1], y_path[-1], dt)
+            r = self.interest_rate(dt, x, y, t_start + step*dt)
+            x_path.append(x)
+            y_path.append(y)
+            r_path.append(r)
+        x_path = torch.stack(x_path, dim=1)
+        y_path = torch.stack(y_path, dim=1)
+        r_path = torch.stack(r_path, dim=1)
+        return r_path, x_path, y_path
+
 
 if __name__ == "__main__":
     config = AnnuityConfig()
     device = 'cpu'
     dtype = torch.float32
-    n_samples = 100 
+    n_samples = int(1e4) 
     model = TwoFactorOrnsteinUhlenbeckModel(config=config, seed=42, device=device, dtype=dtype)
     start_x = start_y = torch.zeros(n_samples, device=device, dtype=dtype)
     x, y = model.coupled_increment(start_x, start_y, dt=1)
@@ -63,3 +125,21 @@ if __name__ == "__main__":
     plt.ylabel('Density')
     plt.title('Histogram of x and y')
     plt.savefig('ornstein_uhlenbeck_histogram.png')
+    model = G2PPModel(config=config, seed=42, device=device, dtype=dtype)
+    r = model.interest_rate(dt=100, x_start=start_x, y_start=start_y, t_start=0)
+    plt.figure()
+    sns.histplot(r.cpu().numpy(), kde=True, color='green', label='r', stat='density')
+    plt.legend()
+    plt.xlabel('Value')
+    plt.ylabel('Density')
+    plt.title(f'Histogram of interest rate at time t={100}')
+    plt.savefig('g2pp_histogram.png')
+    n_steps = 101 
+    r, x, y = model.interest_rate_path(n_steps=n_steps, x_start=start_x, y_start=start_y, t_start=0, dt=1)
+    plt.figure()
+    plt.plot(r[0:10, :].cpu().numpy().T)
+    plt.xlabel('Time step')
+    plt.ylabel('Interest rate')
+    plt.xlim(0, n_steps-1)
+    plt.title('Sample paths of interest rate')
+    plt.savefig('g2pp_sample_paths.png')
